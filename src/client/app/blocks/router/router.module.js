4c9b40ad6752bb0e56b76a8f708726e5a88178fc
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-05-21 15:28:33 +0100

    add doc reference to component router

--- src/client/app/blocks/router/router.module.js
+++ src/client/app/blocks/router/router.module.js
@@ -2,6 +2,8 @@
   'use strict';
 
   angular.module('blocks.router', [
+    // AngularJS Component Router
+    // https://docs.angularjs.org/guide/component-router
     'ngComponentRouter',
     'blocks.logger'
   ]);
